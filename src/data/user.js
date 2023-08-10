bac357021ad0c0b582fe3821eee1e58c4a6d23cd
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-08-10 17:27:34 +0200

    home-projects

--- src/data/user.js
+++ src/data/user.js
@@ -32,48 +32,45 @@ const INFO = {
 
   projects: [
     {
-      title: "Project 1",
-      description:
-        "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+      title: "MERN Stack - Social Media app ",
+      description: "Holberton school full stack graduation project. It's a social network app based on the MERN stack: MongoDb, Express,React, Node.js. Complet parthner project, we built both back-end & front-end.",
       logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
       linkText: "View Project",
-      link: "https://github.com",
+      link: "https://github.com/rafyc/MERN_Social_Media",
     },
 
     {
-      title: "Project 2",
+      title: "Web app in JavaScript",
       description:
-        "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+        "In this project, I implement 3 web pages with Bootstrap ans Javascript. I didn't have a lot of intructions, I were free to implement it the way that I want - the objective is simple: Have fully functional web pages that look the same as the designer file.",
+      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
       linkText: "View Project",
-      link: "https://github.com",
+      link: "https://github.com/rafyc/holbertonschool-smiling-school-javascript/",
     },
 
     {
-      title: "Project 3",
+      title: "Implement a design from scratch",
       description:
-        "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+        "In this project, i implemented from scratch, without any library, a web page. I used all HTML/CSS/Accessibility/Responsive design knowledges that i learned previously",
       logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
       linkText: "View Project",
-      link: "https://github.com",
+      link: "https://github.com/rafyc/holbertonschool-headphones/tree/main",
     },
 
     {
-      title: "Project 4",
-      description:
-        "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+      title: "Air BNB Clone",
+      description: "This project was in 3 staps : Wrote a command interpreter to manage your AirBnB objects, manipulate data without a visual interface, A website (the front-end) that shows the final product to everybody: static and dynamic, A database or files that store data (data = objects), An API that provides a communication interface between the front-end and your data (retrieve, create, delete, update them)",
+      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/python/python.png",
       linkText: "View Project",
-      link: "https://github.com",
+      link: "https://github.com/rafyc/holbertonschool-AirBnB_clone_v4",
     },
 
     {
-      title: "Project 5",
-      description:
-        "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+      title: "Printf",
+      description: "This program is a pseudo- recreation of the C standard library function, printf. A formatted output conversion C program completed as part of the low-level programming and algorithm track at Holberton School.",
+      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/c/c.png",
       linkText: "View Project",
-      link: "https://github.com",
+      link: "https://github.com/rafyc/_printf",
     },
   ],
 };
